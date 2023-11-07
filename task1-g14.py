c4d7ddff916e343bc55b98527b0c8f9f10515b55
Chandrashekhar R <cr2007@hw.ac.uk>
2023-11-07 23:36:09 +0400

    Add author information to Task 1 script

--- task1-g14.py
+++ task1-g14.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """F20CN CW2 Task 1
    Nov 2023
-   Group Members:
-   • Chandrashekhar Ramaprasad - cr2007
-   • Oluwadolabomi Faith Muraino - fm2020
+
+   @author: Chandrashekhar Ramaprasad - cr2007
 """
