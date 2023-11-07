a96068ea96f5ae2dd42ae79fffd5e46f6661674c
Chandrashekhar R <cr2007@hw.ac.uk>
2023-11-07 23:37:41 +0400

    [Task1] Adds function to create a random sequence
    For Public Key generation

--- task1-g14.py
+++ task1-g14.py
@@ -4,3 +4,25 @@
 
    @author: Chandrashekhar Ramaprasad - cr2007
 """
+
+import random
+
+######## Creating a Public Key ########
+
+def create_random_sequence(sequence_length: int = 8) -> list:
+    """Creates a random sequence of n bits, each being subseqently
+    """
+
+    # Create a random sequence of n bits
+    random_sequence: list = [ random.randint(1,100) ]
+
+    # Add random bits to the sequence until it reaches the desired length
+    while len(random_sequence) < sequence_length:
+        # The next element is the sum of the previous element and a random number between 1 and 100
+        next_element:int = sum(random_sequence) + random.randint(1,100)
+
+        # Add the next element to the sequence
+        random_sequence.append(next_element)
+
+    # Return the random sequence
+    return random_sequence
