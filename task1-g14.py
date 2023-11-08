54c38364f8e5b196f1570ae4b50108cca2719818
Chandrashekhar R <cr2007@hw.ac.uk>
2023-11-08 19:09:01 +0400

    Add prime number generation function

--- task1-g14.py
+++ task1-g14.py
@@ -6,6 +6,7 @@
 """
 
 import random
+import math
 
 ######## Creating a Public Key ########
 
@@ -34,6 +35,39 @@ def create_random_sequence(sequence_length: int = 8) -> list:
     # Return the random sequence
     return random_sequence
 
+def is_prime(number: int) -> bool:
+    """
+    Checks if a number is prime or not.
+
+    Parameters:
+    number (int): The number to be checked.
+
+    Returns:
+    bool: True if the number is prime, False otherwise.
+    """
+    # 1 and 2 are not considered prime numbers in this implementation
+    if number in [1,2]:
+        return False
+
+    # Check divisibility for all numbers up to the square root of the input number
+    for i in range(2, int(math.sqrt(number))+1):
+        # If the number is divisible by any number in the range, it's not prime
+        if number % i == 0:
+            return False
+
+    # If no divisors found, the number is prime
+    return True
+
+
 if __name__ == "__main__":
     # Create a random sequence of integers
     e: list = create_random_sequence()
+
+    # Choose a random prime number
+    q: int = 2 * e[-1] + random.randint(1,random.randint(100,1000) )
+    while not is_prime(q): # check if q is prime
+        # if not, choose another random prime number
+        q = 2 * e[-1] + random.randint(1,random.randint(100,1000) )
+
+    print(f"Random sequence 'e': {e}")
+    print(f"q: {q}")
