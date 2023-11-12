c7f5efb8dba7919f96f4cb452e84aead68599b07
Chandrashekhar R <cr2007@hw.ac.uk>
2023-11-13 00:03:53 +0400

    Adds tests for the Python Code
    Currently for is_prime, but more to come

---
+++ task1_g14_test.py
@@ -0,0 +1,22 @@
+import pytest
+
+from task1_g14 import is_prime
+
+class TestPrimeNumber:
+    value = 0
+
+    def testPrimeNumber1(self):
+        self.value = 7
+        assert is_prime(self.value) == True
+
+    def testPrimeNumber2(self):
+        self.value = 11
+        assert is_prime(self.value) == True
+
+    def testPrimeNumber3(self):
+        self.value = 467586793
+        assert is_prime(self.value) == True
+
+    def testPrimeNumber4(self):
+        self.value = 53164652
+        assert is_prime(self.value) == False
