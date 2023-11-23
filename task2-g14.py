43ba288c237d2326ac026dd0dd85244164523dab
Chandrashekhar R <cr2007@hw.ac.uk>
2023-11-23 21:23:37 +0000

    Task 2: Removes trailing whitespace

--- task2-g14.py
+++ task2-g14.py
@@ -5,8 +5,8 @@ import ipaddress  # For IP address validation
 
 def address_in_range(check_address, range_address):
     try:
-        if '-' in range_address:#check if a range is specified (the hyphen exists)
-            #split the range addresses into start and end addresses and converts them to ip addresses objects.
+        if '-' in range_address:  # Checks if a range is specified (the hyphen exists)
+            # Splits the range addresses into start and end addresses and converts them to IP addresses objects.
             start_address, end_address = map(ipaddress.ip_address, range_address.split('-'))
               #check if the ip address is within the specified range.
             return start_address <= ipaddress.ip_address(check_address) <= end_address
@@ -45,8 +45,8 @@ class Firewall:
          #add new rule to list
         self.rules[rule_id] = FirewallRule(rule_id, direction, address)
         self.save_rules() #save the new set of rules
-        
-    def remove_rule(self, rule_id, direction=None):#remove rule from firewall
+
+    def remove_rule(self, rule_id, direction=None): # Remove rule from firewall
         if rule_id not in self.rules:
             return "Error: Rule does not exist."
         else:
@@ -61,7 +61,7 @@ class Firewall:
         for key, value in sorted(self.rules.items()):
              #Skip this rule if it doesn't have the rule ID we're looking for.
             if rule_id is not None and key != rule_id:
-                continue 
+                continue
             # Skip this rule if it doesn't go in the direction we're looking for.
             if direction is not None and not (value.direction == direction or value.direction == 'both'):
                 continue
@@ -70,7 +70,7 @@ class Firewall:
                 continue
             listed_rules.append((key, value.direction, value.address))
         return listed_rules
-         # If the rule passes all checks, add it to our list.
+        # If the rule passes all checks, add it to our list.
 
     def save_rules(self):
         with open('firewall_rules.json', 'w') as file:
@@ -109,7 +109,7 @@ def main():
     fw = Firewall()
 
     try:
-        # Check if any command-line arguments are provided. 
+        # Check if any command-line arguments are provided.
         if len(sys.argv) > 1:
             command = sys.argv[1]  # Get the command from the first argument.
            # Check if the command is 'add' to add a new rule.
@@ -121,7 +121,7 @@ def main():
                     if not fw.validate_ip_address(address):
                         print("Invalid IP address or range.")
                         sys.exit(1) # Exit if the address is invalid.
-                     # Add the rule with the default ID and direction.    
+                     # Add the rule with the default ID and direction.
                     fw.add_rule(1, "both", address)
                     # Print confirmation of the added rule.
                     print(f"Rule added with default ID and direction: {json.dumps({'rule_id': 1, 'direction': 'both', 'address': address})}")
@@ -148,7 +148,7 @@ def main():
                     if not fw.validate_ip_address(address):
                         print("Invalid IP address or range.")
                         sys.exit(1) # Exit if the address is invalid.
-                      # Add the rule with the given details.    
+                      # Add the rule with the given details.
                     fw.add_rule(rule_id, direction, address)
                       # Print confirmation of the added rule.
                     print(f"Rule added: {json.dumps({'rule_id': rule_id, 'direction': direction, 'address': address})}")
@@ -157,7 +157,7 @@ def main():
                       # Print an error message if the command format is incorrect.
                     print("Error: Invalid arguments for add command.")
                     sys.exit(1)   # Exit the program.
-            
+
             # Check if the command is 'remove' to remove a rule.
             elif command == 'remove':
                 # Check if at least a rule ID is provided.
@@ -182,11 +182,11 @@ def main():
                 rule_id = None
                 direction = None
                 address = None
-                
+
                  # Iterate through additional arguments to set the filter criteria.
                 for arg in sys.argv[2:]:
                     if arg.isdigit():  # Check if the argument is a digit (for rule ID).
-                    
+
                         rule_id = int(arg) # Set the rule ID for filtering.
                     elif arg in ['-in', '-out']:  # Check if the argument is '-in' or '-out'.
                         direction = 'in' if arg == '-in' else 'out' # Set the direction for filtering.
@@ -196,7 +196,7 @@ def main():
                         # Print an error message for invalid arguments.
                         print(f"Invalid argument for list command: {arg}")
                         sys.exit(1) # Exit the program.
-                
+
                 # List the rules based on the given filter criteria.
                 for rule in fw.list_rules(rule_id, direction, address):
                     print(f"Rule {rule[0]}: Direction: [{rule[1]}], Address: [{rule[2]}]")
@@ -215,4 +215,4 @@ def main():
         sys.exit(1) # Exit the program if an IndexError occurs.
 
 if __name__ == "__main__":
-    main()     
+    main()
