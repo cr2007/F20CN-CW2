4135944db212b8737ef220a8f5fd37cf12ba94c7
Chandrashekhar R <cr2007@hw.ac.uk>
2024-01-18 14:23:25 +0400

    Fixes issue with Task1 Pytest module import
    Uses 'importlib' library to import the file module easily
    And use the functions within my module in the test file
    (Gotta love Copilot frfr)

--- task1_g14_test.py
+++ task1_g14_test.py
@@ -1,11 +1,12 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring, missing-class-docstring
 # pylint: disable=line-too-long
+import importlib
 import pytest
 
-from task1_g14 import PrivateKey, PublicKey, Encryption, Decryption
+task1_g14 = importlib.import_module("task1-g14")
 
 class TestPrimeNumber:
-    priv_key = PrivateKey()
+    priv_key = task1_g14.PrivateKey()
 
     def testPrimeNumber1(self):
         value = 7
@@ -36,7 +37,7 @@ class TestPrimeNumber:
         assert self.priv_key.is_prime(value)
 
 class TestCreateRandomSequence:
-    __priv_key = PrivateKey()
+    __priv_key = task1_g14.PrivateKey()
 
     def test_create_random_sequence1(self):
         sequence = self.__priv_key.create_random_sequence(5)
@@ -70,29 +71,29 @@ class TestCreateRandomSequence:
 class TestEncryptionDecryption:
 
     def test_encryption_decryption1(self):
-        priv_key = PrivateKey()
+        priv_key = task1_g14.PrivateKey()
         private_key = priv_key.calculate_private_key()
 
-        pub_key = PublicKey(private_key)
+        pub_key = task1_g14.PublicKey(private_key)
         public_key = pub_key.calculate_public_key()
 
         # 'Group 14' in binary
         plain_text = ['01000111', '01110010', '01101111', '01110101', '01110000', '00100000',
                       '00110001', '00110100']
 
-        encrypt = Encryption(public_key, plain_text)
+        encrypt = task1_g14.Encryption(public_key, plain_text)
         cipher_text = encrypt.encryption()
 
-        decrypt = Decryption(cipher_text, private_key)
+        decrypt = task1_g14.Decryption(cipher_text, private_key)
         decrypted_text = decrypt.decryption()
 
         assert decrypted_text == 'Group 14'
 
     def test_encryption_decryption2(self):
-        priv_key = PrivateKey()
+        priv_key = task1_g14.PrivateKey()
         private_key = priv_key.calculate_private_key()
 
-        pub_key = PublicKey(private_key)
+        pub_key = task1_g14.PublicKey(private_key)
         public_key = pub_key.calculate_public_key()
 
         # 'Group 14' in binary
@@ -104,19 +105,19 @@ class TestEncryptionDecryption:
                       '01100001', '01111010', '01111001', '00100000', '01100100', '01101111',
                       '01100111', '00101110']
 
-        encrypt = Encryption(public_key, plain_text)
+        encrypt = task1_g14.Encryption(public_key, plain_text)
         cipher_text = encrypt.encryption()
 
-        decrypt = Decryption(cipher_text, private_key)
+        decrypt = task1_g14.Decryption(cipher_text, private_key)
         decrypted_text = decrypt.decryption()
 
         assert decrypted_text == 'The brown fox jumps over the lazy dog.'
 
     def test_encryption_decryption3(self):
-        priv_key = PrivateKey()
+        priv_key = task1_g14.PrivateKey()
         private_key = priv_key.calculate_private_key()
 
-        pub_key = PublicKey(private_key)
+        pub_key = task1_g14.PublicKey(private_key)
         public_key = pub_key.calculate_public_key()
 
         # 'Group 14' in binary
@@ -259,19 +260,19 @@ class TestEncryptionDecryption:
                       '01110100', '01110011', '01101000', '01101001', '01101110', '01100101',
                       '00100000', '01101000', '01101001', '01101101', '00101110']
 
-        encrypt = Encryption(public_key, plain_text)
+        encrypt = task1_g14.Encryption(public_key, plain_text)
         cipher_text = encrypt.encryption()
 
-        decrypt = Decryption(cipher_text, private_key)
+        decrypt = task1_g14.Decryption(cipher_text, private_key)
         decrypted_text = decrypt.decryption()
 
         assert decrypted_text == 'You cannot worry about upsetting every person you come across, but you must be selectively cruel. If your superior is a falling star, there is nothing to fear from outshining him. Do not be merciful--your master had no such scruples in his own cold-blooded climb to the top. Gauge his strength. If he is weak, discreetly hasten his downfall: Outdo, outcharm, outsmart him at key moments. If he is very weak and ready to fall, let nature take its course. Do not risk outshining a feeble superior--it might appear cruel or spiteful. But if your master is firm in his position, yet you know yourself to be the more capable, hide your time and be patient. It is the natural course of things that power eventually fades and weakens. Your master will fall someday, and if you play it right, you will outlive and someday outshine him.'
 
     def test_encryption_decryption4(self):
-        priv_key = PrivateKey()
+        priv_key = task1_g14.PrivateKey()
         private_key = priv_key.calculate_private_key()
 
-        pub_key = PublicKey(private_key)
+        pub_key = task1_g14.PublicKey(private_key)
         public_key = pub_key.calculate_public_key()
 
         # 'Group 14' in binary
@@ -384,10 +385,10 @@ class TestEncryptionDecryption:
                       '01100101', '01110011', '00100000', '01111001', '01101111',
                       '01110101', '00101110']
 
-        encrypt = Encryption(public_key, plain_text)
+        encrypt = task1_g14.Encryption(public_key, plain_text)
         cipher_text = encrypt.encryption()
 
-        decrypt = Decryption(cipher_text, private_key)
+        decrypt = task1_g14.Decryption(cipher_text, private_key)
         decrypted_text = decrypt.decryption()
 
         assert decrypted_text == 'Do not be one of the many who mistakenly believe that the ultimate form of power is independence. Power involves a relationship between people; you will always need others as allies, pawns, or even as weak masters who serve as your front. The completely independent man would live in a cabin in the woods--he would have the freedom to come and go as he pleased, but he would have no power. The best you can hope for is that others will grow so dependent on you that you enjoy a kind of reverse independence: Their need for you frees you.'
