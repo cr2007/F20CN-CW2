90d0581c2896e41e2c88cc4f9edfdfb56244d932
Chandrashekhar R <cr2007@hw.ac.uk>
2024-01-18 14:25:07 +0400

    [TEST] Changes method names to snake case styling

--- task1_g14_test.py
+++ task1_g14_test.py
@@ -8,31 +8,31 @@ task1_g14 = importlib.import_module("task1-g14")
 class TestPrimeNumber:
     priv_key = task1_g14.PrivateKey()
 
-    def testPrimeNumber1(self):
+    def test_prime_number1(self):
         value = 7
         assert self.priv_key.is_prime(value)
 
-    def testPrimeNumber2(self):
+    def test_prime_number2(self):
         value = 11
         assert self.priv_key.is_prime(value)
 
-    def testPrimeNumber3(self):
+    def test_prime_number3(self):
         value = 467586793
         assert self.priv_key.is_prime(value)
 
-    def testPrimeNumber4(self):
+    def test_prime_number4(self):
         value = 53164652
         assert not self.priv_key.is_prime(value)
 
-    def testPrimeNumber5(self):
+    def test_prime_number5(self):
         value = 8768755
         assert not self.priv_key.is_prime(value)
 
-    def testPrimeNumber6(self):
+    def test_prime_number6(self):
         value = 2324521457
         assert self.priv_key.is_prime(value)
 
-    def testPrimeNumber7(self):
+    def test_prime_number7(self):
         value = 2848637806241
         assert self.priv_key.is_prime(value)
 
